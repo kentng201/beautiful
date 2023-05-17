12cae95a462e70a56359aa48d0c69580b45e2ebd
Kent Ng <kent.ng201@gmail.com>
2023-05-17 20:19:27 +0800

    init

---
+++ src/commands/biu.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+async function asking(question: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+        readline.question(question, (answer: string) => {
+            resolve(answer);
+        });
+    });
+}
+
+(async () => {
+    // console.log('process args: ', process.argv);
+    const command = await asking(chalk.green(`Choose one of the following options:`)+`
+    1. Import Beau Component(s)
+    2. Get Imported Component List
+    3. Remove Imported Component(s)
+    4. Compile source to exportable component
+Your choice is: `);
+
+    if ([1, 2, 3, 4].includes(Number(command))) {
+        console.log('hi');
+    } else {
+        console.log(chalk.red('Invalid command, dismiss.'));
+    }
+    
+    readline.close();
+    process.exit(0);
+})();
