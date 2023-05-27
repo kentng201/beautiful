9bdffd3ac59459a77dff709eac9e40ce884516d6
Kent Ng <kent.ng201@gmail.com>
2023-05-27 12:25:24 +0800

    validate if

--- src/ast/statements/if.ts
+++ src/ast/statements/if.ts
@@ -2,17 +2,22 @@ import { parseCondition } from "../ModelQueryParser";
 import { StatementObject } from "../StatementParser";
 
 export function verifyIfStatement(line: string) {
-    if (line.startsWith('if') && line.match(/\b(if)\b/g)?.length == 1) {
-    } else if (line.includes(' if')) {
+    if (!line.startsWith('if')) {
         throw new Error(JSON.stringify({
             msg: 'SyntaxError: "if" should be at the beginning of the line',
+            lineNo: undefined,
+        }))
+    }
+    if (line.match(/\b(if)\b/g)?.length != 1) {
+        throw new Error(JSON.stringify({
+            msg: 'SyntaxError: Duplicate identifier "if"',
             lineNo: undefined,
         }))
     }
 }
 
 export function extractIfStatementToObject(line: string) {
-    const expressionWithConditions = line.replace('if ', ''); 
+    const expressionWithConditions = line.replace('if ', '');
     const conditions = parseCondition(expressionWithConditions);
     return new StatementObject('if', '', conditions);
 }
