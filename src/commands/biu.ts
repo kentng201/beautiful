1eed48af878a8629ae7848f2b8656b882c350964
Kent Ng <kent.ng201@gmail.com>
2023-05-27 13:31:56 +0800

    fix ts

--- src/commands/biu.ts
+++ src/commands/biu.ts
@@ -16,14 +16,14 @@ async function asking(question: string): Promise<string> {
 
 (async () => {
     // console.log('process args: ', process.argv);
-    const command = await asking(chalk.green(`Choose one of the following options:`) + `
+    const command = await asking(chalk.green('Choose one of the following options:') + `
     1. Import Beautiful Component(s)
     2. Get Imported Component List
     3. Remove Imported Component(s)
     4. Compile source to exportable component
 Your choice is: `);
 
-    if ([1, 2, 3, 4].includes(Number(command))) {
+    if ([1, 2, 3, 4,].includes(Number(command))) {
         console.log('hi');
     } else {
         console.log(chalk.red('Invalid command, dismiss.'));
