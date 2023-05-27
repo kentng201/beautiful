61cebe8f9c6512293c9a9a25df75e5a3096ed097
Kent Ng <kent.ng201@gmail.com>
2023-05-27 16:23:31 +0800

    update reserved keyword

--- src/ast/reserved/index.ts
+++ src/ast/reserved/index.ts
@@ -1,4 +1,4 @@
-export const operatorKeywords = ['more than', 'less than', 'equal', 'not equal', 'full equal', 'not full equal', 'more than or equal', 'less than or equal', 'is a', 'is an', 'is'];
+export const operatorKeywords = ['more than', 'less than', 'equal', 'not equal', 'full equal', 'not full equal', 'more than or equal', 'less than or equal', 'is a', 'is an', 'is', 'a', 'an'];
 export const logicalOperatorKeywords = ['>=', '<=', '>', '<', '===', '!==', '!=', '=', '~=', 'like', 'between'];
 
 export const arithemticOperatorKeywords = ['add', 'minus', 'multiply', 'divide', 'mod', 'power'];
@@ -8,6 +8,8 @@ export const assignKeywords = ['assign', 'via'];
 
 export const statementKeywords = ['if', 'else', 'for', 'every', 'switch', 'while', 'loop'];
 export type StatementKeyword = typeof statementKeywords[number];
+
+export const statementControlKeywords = ['in', 'as'];
 
 export const loopControlKeywords = ['break', 'continue', 'return'];
 
@@ -20,10 +22,12 @@ export const expressionRegex = /(.*)\s*(==|===|>|>=|<|<=|more than|less than|equ
 export const modelKeywords = ['select', 'load', 'save', 'find', 'order', 'by', 'from', 'where', 'first', 'last', 'limit', 'offset'];
 export type ModelKeyword = typeof modelKeywords[number];
 
-export const reserverdWords = ([] as string[])
-    .concat(statementKeywords)
-    .concat(modelKeywords)
-    .concat(assignKeywords)
-    .concat(loopControlKeywords)
-    .concat(logicaKeywords)
-    .concat(operatorKeywords);
+export const reserverdWords = ([] as string[]).concat(
+    statementKeywords,
+    statementControlKeywords,
+    modelKeywords,
+    assignKeywords,
+    loopControlKeywords,
+    logicaKeywords,
+    operatorKeywords
+);
