650e283a3e882c3695a6c4fec7a99e73ef5b6d5f
Kent Ng <kent.ng201@gmail.com>
2023-05-28 20:45:53 +0800

    add error message for syntax

--- src/commands/feature.ts
+++ src/commands/feature.ts
@@ -1,5 +1,6 @@
 import validate from 'src/scripts/validate';
 import parse from 'src/scripts/parse';
+import chalk from 'chalk';
 // import convert from 'src/scripts/convert';
 
 const command = process.argv[2];
@@ -16,4 +17,13 @@ if (command === 'validate') {
         await parse();
         // await convert();
     })();
+} else {
+    console.log(chalk.red('Invalid command' + (command ? ': ' + command : '')));
+    console.log('Usage: beau <command>');
+    console.log('Available commands:');
+    console.log(chalk.green('    validate') + ' - Validate a .beau file');
+    console.log(chalk.green('    parse') + ' - Parse a .beau file');
+    console.log(chalk.green('    convert') + ' - Convert a .beau file to .js');
+    console.log(chalk.green('    build') + ' - Validate, parse, and convert a .beau file');
+    process.exit(1);
 }
